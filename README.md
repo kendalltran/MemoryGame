5e40b08dc3324ee1def08acd1719e9bb3b4ea3ae
Kendall Tran <kendalltran@gmail.com>
2019-02-18 11:50:11 -0500

    master2

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # MemoryGame
 this game is a game of memory.
 match 2 under 50 seconds and win!
-
-For now only take a look at index.html file, which includes style elements (to be in .css file) and script elements (to be in .js file).
--- js/app.js
+++ js/app.js
@@ -1,105 +1,84 @@
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+let memory_array = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H',];
+let memory_values = [];
+let memory_tile_ids = [];
+let tiles_flipped = 0;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-function newBoard() {
-  tilesFlipped = 0;
-  var output = '';
-  shuffle(memoryArray);
-  for (let i = 0; i < memoryArray.length; i++) {
-    output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memoryArray[i]+'\')"></div>';
-    }
-    document.getElementsByClassName('deck').innerHTML = output;
-}
-
-function memoryFlipTile(tile, val) {
-if(tile.innerHTML == "" && memoryValues.length < 2) {
-  tile.style.background = '#FFF';
-  tile.innerHTML = val;
-  if(memoryValues.length == 0) {
-    memoryValues.push(val);
-    memoryTileIDs.push(tile.id);
-  } else if (memoryValues.length == 1) {
-    memoryValues.push(val);
-    memoryTileIDs.push(tile.id);
-    if(memoryValues[0] == memoryValues[1]){
-      tilesFlipped += 2;
-      //clear both arrays
-      memoryValues = [];
-      memoryTileIDs = [];
-      //check to see if whole board is cleared
-      if(tilesFlipped == memoryArray.length) {
-        alert("board cleared...generating new board");
-        document.getElementByID('deck').innerHTML = "";
-        newBoard();
-      }
-    } else {
-      function flip2Back() {
-        let tile_1 = document.getElementByID(memoryTileIDs[0]);
-        let tile_2 = document.getElementByID(memoryTileIDs[1]);
-        tile_1.style.backgroundColor = "red";
-        tile_2.style.backgroundColor = "red";
-        tile_1.innerHTML = "";
-        tile_2.innerHTML = "";
-        //clear both arrays
-        memoryValues = [];
-        memoryTileIDs = [];
-      }
-      setTimeout(flip2Back, 700);
-    }
+Array.prototype.memory_tile_shuffle = function() {
+  var i = this.length, j, temp;
+  while (--i > 0) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = this[j];
+    this[j] = this[i];
+    this[i] = temp;
   }
 }
+function randomColor() {
+  var color = '#'; // hexadecimal starting symbol
+  var letters = ['FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF']; //Set your colors here
+  color += letters[Math.floor(Math.random() * letters.length)];
+  document.getElementById('memory_board').style.background = color; // Setting the random color on your div element.
+}
+function newBoard() {
+  randomColor();
+  tiles_flipped = 0;
+  var output = '';
+  memory_array.memory_tile_shuffle();
+  for(var i = 0; i < memory_array.length; i++) {
+    output += '<div class="card" id="tile_'+i+'"onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+}
+  var timeleft = 50;
+  var downloadTimer = setInterval(function(){
+    document.getElementById("timer").innerHTML = timeleft + " seconds remaining";
+    timeleft -= 1;
+    if(timeleft <= -1){
+      clearInterval(downloadTimer);
+      document.getElementById("timer").innerHTML = "time is up";
+    }
+  }, 1000);
+  document.getElementById('memory_board').innerHTML = output;
 }
 
-let memoryArray = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D']
-let memoryValues = [];
-let memoryTileIDs = [];
+function shake () {
+  let card = document.querySelector(".card");
+  console.log(card);
+  console.log('does this display');
+}
+function memoryFlipTile(tile, value) {
+    if (memory_values.length < 2) {
+      tile.style.background = '#FFF';
+      tile.innerHTML = value;
+      if (memory_values.length === 0) {
+        memory_values.push(value);
+        memory_tile_ids.push(tile.id);
+        // console.log(memory_values); console.log(memory_tile_ids);
+      } else if (memory_values.length === 1) {
+          memory_values.push(value);
+          memory_tile_ids.push(tile.id);
+          // console.log(memory_values); console.log(memory_tile_ids);
+          if (memory_values[0] === memory_values[1]) {
+            // alert(`you have gotten ${memory_values[0]}!`);
+            memory_values = []; memory_tile_ids = [];
+            tiles_flipped += 2;
+          } else {
+            // console.log('does not match');
+            function flip2Back() {
+                var tile0 = document.getElementById(memory_tile_ids[0]);
+                var tile1 = document.getElementById(memory_tile_ids[1]);
+                // console.log('does not match2'); console.log(tile0); console.log(tile1);
+                tile0.style.background = 'black'; tile1.style.background = 'black';
+                tile0.innerHTML = ''; tile1.innerHTML = '';
+                // clear both arrays
+                memory_values = []; memory_tile_ids = [];
+                }
+              setTimeout(flip2Back, 700);
+            }
+        }
+    }
+    if (tiles_flipped === memory_array.length) { //LOOK INTO THIS:  something about get/set and queues javascript vs html......
+      document.getElementById('memory_board').innerHTML = "";
+      setTimeout(alert('you win!'), 1400)
+      newBoard();
+    }
+}
+
 newBoard();
-
-
-shuffle(memoryArray);
-
-// ^^you have to invoke this function, not just declare it dummy;
-
-// let x = shuffle([1, 2, 3, 4]);
-
-const aHeading = document.querySelector('.A');
-const aTextAdd = '<h2>A</h2>';
-aHeading.insertAdjacentHTML('afterbegin', aTextAdd)
-
-
-// const newSpan = document.createElement('span');
-// newSpan.textContent = 'blahgaty blubity blue';
-// document.body.appendChild(newSpan);
-
-const mainHeading = document.querySelector('ul');
-
-mainHeading.addEventListener('click', function () {
-  console.log('The icon was clicked!');
-  document.body.appendChild(newSpan);
-});
-
-const mainHeading2 = document.querySelector('.jets');
-const htmlTextToAdd = '<h2>Skydiving is fun!</h2>';
-
-mainHeading2.insertAdjacentHTML('beforeend', htmlTextToAdd);
-
-// const cardName = document.getElementsByClassName('card');
-// console.log(cardName);
-
-const allCards = document.querySelectorAll('.card');
-for( let i = 0; i < allCards.length; i++){
-    console.dir(allCards[i]);
-}
-
-// DOM is globally accessible by JavaScript code using the document object
