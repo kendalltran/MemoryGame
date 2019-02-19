fb6495a385f1b3aaf7d708dff0a93f9b2a5d7585
Kendall Tran <kendalltran@gmail.com>
2019-02-18 22:30:37 -0800

    master2

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# MemoryGame
-this game is a game of memory.
-match 2 under 50 seconds and win!
+Memory Game
+---This project, titled "Memory Game", is a Front End Development project that challenges the participant to complete the game under 50 seconds, all the while minimizing the amount of clicks/tries in order to maximize the 'star' score, with a max of 10 stars.  
+
+---This code utilizes 3 files to be functional running code:
+  index.html
+  app.css
+  app.js
+
+---This code version is master2 and is available publicly on GitHub.
--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@ let memory_array = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 
 let memory_values = [];
 let memory_tile_ids = [];
 let tiles_flipped = 0;
+let click_attempts = 0;
+let t = 50;
+let stars = 10;
 
+// shuffle function added to prototype
 Array.prototype.memory_tile_shuffle = function() {
   var i = this.length, j, temp;
   while (--i > 0) {
@@ -12,72 +16,102 @@ Array.prototype.memory_tile_shuffle = function() {
     this[i] = temp;
   }
 }
+
+// function creates a random background color
 function randomColor() {
   var color = '#'; // hexadecimal starting symbol
   var letters = ['FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF']; //Set your colors here
   color += letters[Math.floor(Math.random() * letters.length)];
   document.getElementById('memory_board').style.background = color; // Setting the random color on your div element.
 }
+
+// function creates new board
 function newBoard() {
+        //provides restart option
+        document.getElementById("restart").addEventListener("click", function() {
+          location.reload();
+        });
+        //timer logic
+        let sTime = setInterval(function() {
+          if (t != 0) {
+            document.getElementById("timer").style.color = "black";
+            document.getElementById("timer").style.fontWeight = "lighter";
+            document.getElementById("timer").innerHTML = t + " seconds remain";
+            if (t <= 10 ) {
+              document.getElementById("timer").style.color = "red";
+              document.getElementById("timer").style.fontWeight = "bolder";
+            }
+            t = t - 1;
+          } else {
+            t = 50;
+            document.getElementById("timer").innerHTML = "0 seconds remain";
+            alert("Time is up.  Press 'OK' to start next game!")
+            location.reload();
+          }
+        }, 1000);
   randomColor();
   tiles_flipped = 0;
+  click_attempts = 0;
   var output = '';
+  //shuffle invocation
   memory_array.memory_tile_shuffle();
   for(var i = 0; i < memory_array.length; i++) {
     output += '<div class="card" id="tile_'+i+'"onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
 }
-  var timeleft = 50;
-  var downloadTimer = setInterval(function(){
-    document.getElementById("timer").innerHTML = timeleft + " seconds remaining";
-    timeleft -= 1;
-    if(timeleft <= -1){
-      clearInterval(downloadTimer);
-      document.getElementById("timer").innerHTML = "time is up";
-    }
-  }, 1000);
   document.getElementById('memory_board').innerHTML = output;
 }
 
-function shake () {
-  let card = document.querySelector(".card");
-  console.log(card);
-  console.log('does this display');
-}
+// function of flipping and clicking
 function memoryFlipTile(tile, value) {
+    click_attempts++;
+    if (click_attempts <= 20) {stars = 10}
+      else if (click_attempts <= 25) {stars = 9; document.getElementById('star10').style.display = "none";}
+      else if (click_attempts <= 30) {stars = 8; document.getElementById('star9').style.display = "none";}
+      else if (click_attempts <= 35) {stars = 7; document.getElementById('star8').style.display = "none";}
+      else if (click_attempts <= 40) {stars = 6; document.getElementById('star7').style.display = "none";}
+      else if (click_attempts <= 45) {stars = 5; document.getElementById('star6').style.display = "none";}
+      else if (click_attempts <= 50) {stars = 4; document.getElementById('star5').style.display = "none";}
+      else if (click_attempts <= 55) {stars = 3; document.getElementById('star4').style.display = "none";}
+      else if (click_attempts <= 60) {stars = 2; document.getElementById('star3').style.display = "none";}
+      else {stars = 1; document.getElementById('star2').style.display = "none";}
+      document.getElementById("move-counter").innerHTML = `Move Counter: ${click_attempts}`;
     if (memory_values.length < 2) {
       tile.style.background = '#FFF';
       tile.innerHTML = value;
       if (memory_values.length === 0) {
         memory_values.push(value);
         memory_tile_ids.push(tile.id);
-        // console.log(memory_values); console.log(memory_tile_ids);
       } else if (memory_values.length === 1) {
           memory_values.push(value);
           memory_tile_ids.push(tile.id);
-          // console.log(memory_values); console.log(memory_tile_ids);
           if (memory_values[0] === memory_values[1]) {
-            // alert(`you have gotten ${memory_values[0]}!`);
             memory_values = []; memory_tile_ids = [];
             tiles_flipped += 2;
           } else {
-            // console.log('does not match');
             function flip2Back() {
                 var tile0 = document.getElementById(memory_tile_ids[0]);
                 var tile1 = document.getElementById(memory_tile_ids[1]);
-                // console.log('does not match2'); console.log(tile0); console.log(tile1);
                 tile0.style.background = 'black'; tile1.style.background = 'black';
                 tile0.innerHTML = ''; tile1.innerHTML = '';
-                // clear both arrays
                 memory_values = []; memory_tile_ids = [];
                 }
               setTimeout(flip2Back, 700);
             }
         }
     }
-    if (tiles_flipped === memory_array.length) { //LOOK INTO THIS:  something about get/set and queues javascript vs html......
+    //win condition
+    if (tiles_flipped === memory_array.length) {
+      let modal = document.getElementById("modal");
+      modal.style.display = "block";
+      let modalPara = document.getElementById("modal-statement");
+      modalPara.innerHTML = `<p>you have won with  ${click_attempts} click attempts (scoring ${stars} out of 10  stars) and ${t} seconds remaining!  Click on X to re-play!</p>`;
+      document.getElementById("timer").style.display = "none";
+      t = -1;  //timer logic hack
       document.getElementById('memory_board').innerHTML = "";
-      setTimeout(alert('you win!'), 1400)
-      newBoard();
+      let span = document.getElementsByClassName("close")[0];
+      span.onclick = function() {
+        location.reload();
+      };
     }
 }
 
